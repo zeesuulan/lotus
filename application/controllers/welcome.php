494da086035247000e924c07adc2f89a3f0e8219
兰斌 <lanbin1987@gmail.com>
2014-11-04 13:01:02 +0800

    init

---
+++ application/controllers/welcome.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+
+		$this->load->library('Excel');
+		$this->load->library('Excel/iofactory');
+
+		$objPHPExcel = new PHPExcel();
+		$objPHPExcel->getProperties()->setTitle("title")
+		                 ->setDescription("description");
+
+		// Assign cell values
+		$objPHPExcel->setActiveSheetIndex(0);
+		$objPHPExcel->getActiveSheet()->setCellValue('A1', 'cell value here');
+
+		// Save it as an excel 2003 file
+		$objWriter = IOFactory::createWriter($objPHPExcel, 'Excel5');
+		$objWriter->save("nameoffile.xls");
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
